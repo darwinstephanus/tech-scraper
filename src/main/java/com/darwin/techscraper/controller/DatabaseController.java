13f2cc10c1c52ad57a6c7c1a085f8e388d973e56
Darwin Stephanus <darwin.stephanus@gmail.com>
2020-10-02 22:04:52 -0400

    add cors

--- src/main/java/com/darwin/techscraper/controller/DatabaseController.java
+++ src/main/java/com/darwin/techscraper/controller/DatabaseController.java
@@ -19,16 +19,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class DatabaseController {
 
     @Autowired
